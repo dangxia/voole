c9edb372d5337d66c5b294880ebbd9f9a4281298
xuehuihe.java <xuehuihe.java@gmail.com>
2014-07-31 00:11:32 +0800

    ds

---
+++ hobbit-projects/hobbit-mapreduce-projects/hobbit-mapreduce-test/src/main/java/com/voole/hobbit/mapreduce/test/TestMapReduce3.java
@@ -0,0 +1,100 @@
+package com.voole.hobbit.mapreduce.test;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.avro.Schema;
+import org.apache.avro.Schema.Type;
+import org.apache.avro.mapred.AvroCollector;
+import org.apache.avro.mapred.AvroJob;
+import org.apache.avro.mapred.AvroMapper;
+import org.apache.avro.mapred.AvroReducer;
+import org.apache.avro.mapred.Pair;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.voole.hobbit.avro.termial.OrderPlayBgnReqV2;
+
+public class TestMapReduce3 extends Configured implements Tool {
+	private final SimpleDateFormat df = new SimpleDateFormat(
+			"yyyy-MM-dd-HH-mm-ss");
+	private static final Logger logger = LoggerFactory
+			.getLogger(TestMapReduce3.class);
+
+	public static class TestMapper3
+			extends
+			AvroMapper<OrderPlayBgnReqV2, Pair<CharSequence, OrderPlayBgnReqV2>> {
+		@Override
+		public void map(OrderPlayBgnReqV2 datum,
+				AvroCollector<Pair<CharSequence, OrderPlayBgnReqV2>> collector,
+				Reporter reporter) throws IOException {
+			collector.collect(new Pair<CharSequence, OrderPlayBgnReqV2>(datum
+					.getSessID(), datum));
+		}
+	}
+
+	public static class TestReducer3
+			extends
+			AvroReducer<CharSequence, OrderPlayBgnReqV2, Pair<CharSequence, Integer>> {
+		@Override
+		public void reduce(CharSequence key,
+				Iterable<OrderPlayBgnReqV2> values,
+				AvroCollector<Pair<CharSequence, Integer>> collector,
+				Reporter reporter) throws IOException {
+			int count = 0;
+			for (OrderPlayBgnReqV2 OrderPlayBgnReqV2 : values) {
+				count++;
+			}
+			collector.collect(new Pair<CharSequence, Integer>(key, count));
+		}
+	}
+
+	@Override
+	public int run(String[] args) throws Exception {
+		JobConf conf = new JobConf(getConf(), TestMapReduce3.class);
+		conf.setJobName("test_avro_map_reduce");
+
+		FileInputFormat.setInputPaths(conf,
+				"/kafka/t_playbgn_v2/hourly/2014/07/30/19");
+		// FileInputFormat.addInputPath(conf, new Path(
+		// "/kafka/t_playend_v2/hourly/2014/07/30/19"));
+		FileOutputFormat.setOutputPath(conf,
+				new Path("/tmp/hexh/" + df.format(new Date())));
+
+		AvroJob.setMapperClass(conf, TestMapper3.class);
+		AvroJob.setReducerClass(conf, TestReducer3.class);
+
+		// Note that AvroJob.setInputSchema and AvroJob.setOutputSchema set
+		// relevant config options such as input/output format, map output
+		// classes, and output key class.
+
+		AvroJob.setInputSchema(conf, OrderPlayBgnReqV2.getClassSchema());
+		AvroJob.setMapOutputSchema(conf, Pair.getPairSchema(
+				Schema.create(Type.STRING), OrderPlayBgnReqV2.getClassSchema()));
+		AvroJob.setOutputSchema(
+				conf,
+				Pair.getPairSchema(Schema.create(Type.STRING),
+						Schema.create(Type.INT)));
+		JobClient.runJob(conf);
+		return 0;
+	}
+
+	public static void main(String[] args) throws Exception {
+		System.setProperty("HADOOP_USER_NAME", "root");
+		int res = ToolRunner.run(new Configuration(), new TestMapReduce3(),
+				args);
+		System.exit(res);
+	}
+
+}
