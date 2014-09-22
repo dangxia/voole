d27dde45b2505759f08b9324f7abd3c3f68fe1c5
xuehuihe <xuehuihe.java@gmail.com>
2014-09-22 15:20:56 +0800

    sd

--- hobbit2-project/hobbit2-hadoop-project/hobbit2-hive-order/src/main/java/com/voole/hobbit2/hive/order/HiveOrderHDFSUtils.java
+++ hobbit2-project/hobbit2-hadoop-project/hobbit2-hive-order/src/main/java/com/voole/hobbit2/hive/order/HiveOrderHDFSUtils.java
@@ -154,24 +154,23 @@ public class HiveOrderHDFSUtils {
 
 	public static long readPrevCamusExecTime(JobContext job,
 			Optional<Path> prevExecPath) throws IOException {
-		// if (!prevExecPath.isPresent()) {
-		// return 0l;
-		// }
-		// FileSystem fs = FileSystem.get(job.getConfiguration());
-		// Path prevCamusLastExecStartTimeFile = new Path(prevExecPath.get(),
-		// HiveOrderMetaConfigs.PREV_CAMUS_EXEC_TIME_FILE_NAME);
-		// Preconditions.checkArgument(fs.exists(prevCamusLastExecStartTimeFile),
-		// "PREV_CAMUS_EXEC_TIME_FILE_NAME does not exist");
-		// Reader r = new SequenceFile.Reader(job.getConfiguration(),
-		// SequenceFile.Reader.file(prevCamusLastExecStartTimeFile));
-		// LongWritable prevCamusLastExecStartTimeWritable = new LongWritable();
-		// long lastStartTime = 0;
-		// if (r.next(NullWritable.get(), prevCamusLastExecStartTimeWritable)) {
-		// lastStartTime = prevCamusLastExecStartTimeWritable.get();
-		// }
-		// r.close();
-		// return lastStartTime;
-		return 1411369427858l;
+		if (!prevExecPath.isPresent()) {
+			return 0l;
+		}
+		FileSystem fs = FileSystem.get(job.getConfiguration());
+		Path prevCamusLastExecStartTimeFile = new Path(prevExecPath.get(),
+				HiveOrderMetaConfigs.PREV_CAMUS_EXEC_TIME_FILE_NAME);
+		Preconditions.checkArgument(fs.exists(prevCamusLastExecStartTimeFile),
+				"PREV_CAMUS_EXEC_TIME_FILE_NAME does not exist");
+		Reader r = new SequenceFile.Reader(job.getConfiguration(),
+				SequenceFile.Reader.file(prevCamusLastExecStartTimeFile));
+		LongWritable prevCamusLastExecStartTimeWritable = new LongWritable();
+		long lastStartTime = 0;
+		if (r.next(NullWritable.get(), prevCamusLastExecStartTimeWritable)) {
+			lastStartTime = prevCamusLastExecStartTimeWritable.get();
+		}
+		r.close();
+		return lastStartTime;
 	}
 
 	public static Optional<Path> getPrevExecPath(Configuration conf,
