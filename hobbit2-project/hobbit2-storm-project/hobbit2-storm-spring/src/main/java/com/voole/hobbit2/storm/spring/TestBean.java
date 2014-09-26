f7ca65cb9552c18570b3835ff5c2a750cd71a9e3
xuehuihe <xuehuihe.java@gmail.com>
2014-09-26 17:58:20 +0800

    sd

---
+++ hobbit2-project/hobbit2-storm-project/hobbit2-storm-spring/src/main/java/com/voole/hobbit2/storm/spring/TestBean.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2014 BEIJING UNION VOOLE TECHNOLOGY CO., LTD
+ */
+package com.voole.hobbit2.storm.spring;
+
+/**
+ * @author XuehuiHe
+ * @date 2014年9月26日
+ */
+public class TestBean {
+
+}
