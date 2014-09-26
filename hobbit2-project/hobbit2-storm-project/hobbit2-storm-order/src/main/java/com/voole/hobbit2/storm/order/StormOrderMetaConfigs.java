c5ffb08cc8fe19296cb446b4f42ff0c6f1e9eb94
xuehuihe.java <xuehuihe.java@gmail.com>
2014-09-27 01:28:50 +0800

    sd

--- hobbit2-project/hobbit2-storm-project/hobbit2-storm-order/src/main/java/com/voole/hobbit2/storm/order/StormOrderMetaConfigs.java
+++ hobbit2-project/hobbit2-storm-project/hobbit2-storm-order/src/main/java/com/voole/hobbit2/storm/order/StormOrderMetaConfigs.java
@@ -89,8 +89,9 @@ public class StormOrderMetaConfigs {
 
 	public static int getKafkafetchSize() {
 		if (fetchSize == null) {
-			fetchSize = hobbit2Configuration.getInt(
-					KafkaMetaConfigs.KAFKA_BUFFER_SIZE_BYTES, 10240);
+			fetchSize = 10240;
+//			fetchSize = hobbit2Configuration.getInt(
+//					KafkaMetaConfigs.KAFKA_BUFFER_SIZE_BYTES, 10240);
 		}
 		return fetchSize;
 	}
